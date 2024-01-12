ea2f265057a868c204226efc8ea849e98b8d1d71
brightnkhoma <gis-020-20@must.ac.mw>
2024-01-12 18:45:43 +0100

    add preinstallation

--- api/index.js
+++ api/index.js
@@ -4,8 +4,10 @@ import { Server } from 'socket.io'
 import {createServer} from 'node:http'
 import cors from 'cors'
 import { count } from 'node:console'
+import path from 'path'
 
 const app = express();
+const __dirname = path.resolve();
 app.use(cors());
 const server = createServer(app);
 const io = new Server(server,
@@ -52,4 +54,8 @@ const io = new Server(server,
 
     server.listen(4000,()=>{
         console.log('listening on http://localhost:4000');
+    })
+    app.use(express.static(path.join(__dirname,'/chess/dist')))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'client','dist','index.html'));
     })
